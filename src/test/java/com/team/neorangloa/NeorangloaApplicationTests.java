5634b020a30ec9723c9c07fe0737723551f69c7a
Jmmk <jmk525@gmail.com>
2023-05-11 17:22:02 +0900

    Initial commit

---
+++ src/test/java/com/team/neorangloa/NeorangloaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.neorangloa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeorangloaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
