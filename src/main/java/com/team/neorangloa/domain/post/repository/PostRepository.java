17c35765f19be91f134cf7274d5deb3464db3f26
조성현 <agllwy116@naver.com>
2023-05-18 15:22:33 +0900

    포스트 아이디로 조회할 시 삭제 여부를 검사해서 결과를 반환하도록 수정

--- src/main/java/com/team/neorangloa/domain/post/repository/PostRepository.java
+++ src/main/java/com/team/neorangloa/domain/post/repository/PostRepository.java
@@ -2,9 +2,12 @@ package com.team.neorangloa.domain.post.repository;
 
 import com.team.neorangloa.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("SELECT p FROM Post p WHERE p.id = :postId AND p.removed = false ")
     Optional<Post> findPostById(Long postId);
 }
