1e7ce0b9b74d978386bdbfb22f2e551e11d8529e
Jmmk <jmk525@gmail.com>
2023-05-25 11:00:30 +0900

    interceptor WebConfig 파일에 추가

---
+++ src/main/java/com/team/neorangloa/global/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.team.neorangloa.global.config;
+
+import com.team.neorangloa.global.interceptor.LoginInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(loginInterceptor);
+    }
+}
