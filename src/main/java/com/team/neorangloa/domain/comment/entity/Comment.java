920cd7109bab08b3e7325cf67b96aa097cbed01e
조성현 <agllwy116@naver.com>
2023-05-25 11:56:16 +0900

    댓글 테이블에 Post 연결

--- src/main/java/com/team/neorangloa/domain/comment/entity/Comment.java
+++ src/main/java/com/team/neorangloa/domain/comment/entity/Comment.java
@@ -1,8 +1,8 @@
 package com.team.neorangloa.domain.comment.entity;
 
+import com.team.neorangloa.domain.post.entity.Post;
 import com.team.neorangloa.domain.user.entity.User;
 import com.team.neorangloa.global.entity.BaseTimeEntity;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,6 +28,10 @@ public class Comment extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USER_ID", nullable = false)
     private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID", nullable = false)
+    private Post post;
 
 
 }
