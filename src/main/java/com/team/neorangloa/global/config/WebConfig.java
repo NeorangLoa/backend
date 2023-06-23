272d971efd7ff481b3a951f414f6db442d8175a6
조성현 <agllwy116@naver.com>
2023-06-23 18:44:23 +0900

    서버에서 CORS 적용시키는 코드 구현

--- src/main/java/com/team/neorangloa/global/config/WebConfig.java
+++ src/main/java/com/team/neorangloa/global/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -29,7 +30,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
         resolvers.add(loginUserArgumentResolver);
     }
-//    @Override
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+
+    //    @Override
 //    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 //        converters.add(mappingJackson2HttpMessageConverter());
 //    }
