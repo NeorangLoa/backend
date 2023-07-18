b1f522bf1c483ad2b195db0de87c81f6a0e05cdd
Jmmk <jmk525@gmail.com>
2023-07-18 22:24:40 +0900

    캐릭터 정보 가져오는 오픈 api 추가

--- src/main/java/com/team/neorangloa/domain/user/controller/UserController.java
+++ src/main/java/com/team/neorangloa/domain/user/controller/UserController.java
@@ -13,10 +13,13 @@ import com.team.neorangloa.global.annotation.LoginUser;
 import com.team.neorangloa.global.result.ResultCode;
 import com.team.neorangloa.global.result.ResultResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequiredArgsConstructor
@@ -56,5 +59,26 @@ public class UserController {
         }
         userService.updatePassword(logInUserId, updatePasswordRequest);
         return ResponseEntity.ok(ResultResponse.of(ResultCode.UPDATE_USER_PASSWORD_SUCCESS));
+    }
+    @GetMapping("/charactersInfo/{characterName}")
+    public ResponseEntity<String> getCharactersInfo(@RequestHeader("API-Key") String apiKey,
+                                                    @PathVariable String characterName) {
+        RestTemplate restTemplate = new RestTemplate();
+        String apiUrl = "https://developer-lostark.game.onstove.com/characters/" + characterName + "/siblings";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        headers.set(HttpHeaders.AUTHORIZATION, apiKey);
+
+        HttpEntity<?> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.GET, httpEntity, String.class);
+        System.out.println(response);
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getBody());
+
+        // 클라이언트에 반환할 때는 JSON 형식으로 변환하여 반환
+        // response.getBody()는 API에서 받은 데이터를 문자열로 반환
+        return ResponseEntity.ok(response.getBody());
     }
 }
